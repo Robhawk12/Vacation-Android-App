219b2bd9e4858c5fa61a46b129d4e9558d7c1233
Robert Johnson <rjo1275@wgu.edu>
2024-08-12 13:31:17 -0700

    Vacation List UI Adds buttons and onClickListener navigation

--- app/src/main/java/d308/robertjohnson/vacationplanner/UI/MainActivity.java
+++ app/src/main/java/d308/robertjohnson/vacationplanner/UI/MainActivity.java
@@ -2,7 +2,10 @@ package d308.robertjohnson.vacationplanner.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import d308.robertjohnson.vacationplanner.R;
 
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.start_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,VacationsList.class);
+                intent.putExtra("test", "Information");
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/d308/robertjohnson/vacationplanner/UI/VacationsList.java
+++ app/src/main/java/d308/robertjohnson/vacationplanner/UI/VacationsList.java
@@ -1,8 +1,13 @@
 package d308.robertjohnson.vacationplanner.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.strictmode.FragmentStrictMode;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import d308.robertjohnson.vacationplanner.R;
 
@@ -12,5 +17,14 @@ public class VacationsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacations_list);
+        FloatingActionButton fab=findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(VacationsList.this,VacationView.class);
+                startActivity(intent);
+            }
+        });
+        //System.out.println(getIntent().getStringExtra("test"));
     }
 }
