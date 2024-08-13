18adb8068968681aed790c64c9a30b5252279bc1
Robert Johnson <rjo1275@wgu.edu>
2024-08-13 14:36:46 -0700

    B2 Begins adding data for Vaction List view. Adds navigation

--- app/src/main/java/d308/robertjohnson/vacationplanner/UI/VacationsList.java
+++ app/src/main/java/d308/robertjohnson/vacationplanner/UI/VacationsList.java
@@ -5,7 +5,10 @@ import androidx.fragment.app.strictmode.FragmentStrictMode;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -24,7 +27,25 @@ public class VacationsList extends AppCompatActivity {
                 Intent intent= new Intent(VacationsList.this,VacationView.class);
                 startActivity(intent);
             }
+
         });
         //System.out.println(getIntent().getStringExtra("test"));
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.menu_vacation_list, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.sample) {
+            Toast.makeText(VacationsList.this, "put in sample data", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (item.getItemId()==android.R.id.home){
+            this.finish();
+            return true;
+        }
+        return true;
     }
 }
